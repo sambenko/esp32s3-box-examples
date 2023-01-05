8a3d19404b205318b5c6c7c6234dcfd59a33c6fc
sambenko <sam.benko@gmail.com>
2023-01-05 14:34:57 +0100

    refactored display

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,17 @@
 use display_interface_spi::SPIInterfaceNoCS;
 
 use embedded_graphics::{
-    prelude::RgbColor,
-    pixelcolor::Rgb565,
+    prelude::{RgbColor, Point},
     mono_font::{
         ascii::FONT_10X20,
         MonoTextStyleBuilder,
     },
-    prelude::{Point, DrawTarget},
     text::{Alignment, Text},
     Drawable,
 };
 
 use esp32s3_hal::{
-    clock::ClockControl,
+    clock::{ClockControl, CpuClock},
     pac::Peripherals,
     prelude::*,
     spi,
@@ -28,7 +26,7 @@ use esp32s3_hal::{
 
 use esp_println::println;
 
-use mipidsi::{ Orientation, ColorOrder };
+use mipidsi;
 
 #[allow(unused_imports)]
 use esp_backtrace as _;
@@ -39,7 +37,7 @@ use xtensa_lx_rt::entry;
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let mut system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock240MHz).freeze();
 
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
     let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
@@ -56,44 +54,32 @@ fn main() -> ! {
 
     let sclk = io.pins.gpio7;
     let mosi = io.pins.gpio6;
-    let dc = io.pins.gpio4;
-    let bcklght = io.pins.gpio45;
-    let rst = io.pins.gpio48;
     
-    let mut backlight = bcklght.into_push_pull_output();
+    let mut backlight = io.pins.gpio45.into_push_pull_output();
     backlight.set_high().unwrap();
 
     let spi = spi::Spi::new_no_cs_no_miso(
         peripherals.SPI2,
         sclk,
         mosi,
-        100u32.MHz(),
+        4u32.MHz(),
         spi::SpiMode::Mode0,
         &mut system.peripheral_clock_control,
         &clocks,
     );
 
-    let di = SPIInterfaceNoCS::new(spi, dc.into_push_pull_output());
-    let reset = rst.into_push_pull_output();
+    let di = SPIInterfaceNoCS::new(spi, io.pins.gpio4.into_push_pull_output());
+    let reset = io.pins.gpio48.into_push_pull_output();
 
     let mut delay = Delay::new(&clocks);
 
     let mut display = mipidsi::Builder::ili9342c_rgb565(di)
-        .with_display_size(320, 240)
-        .with_framebuffer_size(320, 240)
         .with_orientation(mipidsi::Orientation::PortraitInverted(false))
         .with_color_order(mipidsi::ColorOrder::Rgb)
-        .init(&mut delay, Some(reset))
-        .unwrap();
+        .init(&mut delay, core::prelude::v1::Some(reset))
+    .unwrap();
 
-    display.clear(Rgb565::WHITE).unwrap();
-
-    let espressif_style = MonoTextStyleBuilder::new()
-        .font(&FONT_10X20)
-        .text_color(RgbColor::BLACK)
-        .build();
-
-    Text::with_alignment("HELLO WORLD!", Point::new(160, 120), espressif_style,  Alignment::Center)
+    Text::with_alignment("HELLO WORLD!", Point::new(160, 120), MonoTextStyleBuilder::new().font(&FONT_10X20).text_color(RgbColor::BLACK).build(),  Alignment::Center)
         .draw(&mut display)
         .unwrap();
     
@@ -101,3 +87,4 @@ fn main() -> ! {
 
     loop {}
 }
+                    
