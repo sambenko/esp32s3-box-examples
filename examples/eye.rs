7941968d102995659462961f08b5f12f526d95af
sambenko <sam.benko@gmail.com>
2023-01-11 18:16:37 +0100

    minor edits and fixes

--- examples/eye.rs
+++ examples/eye.rs
@@ -4,15 +4,18 @@
 use display_interface_spi::SPIInterfaceNoCS;
 
 use embedded_graphics::{
-    prelude::RgbColor,
+    prelude::{ RgbColor, DrawTarget, Point },
+    pixelcolor::Rgb565,
     mono_font::{
         ascii::FONT_10X20,
         MonoTextStyleBuilder,
     },
-    prelude::Point,
     text::{Alignment, Text},
     Drawable,
 };
+
+use core::f32::consts::PI;
+use libm::{sin, cos};
 
 use esp32s3_hal::{
     clock::ClockControl,
@@ -25,10 +28,9 @@ use esp32s3_hal::{
     Delay,
 };
 
-use mipidsi::DisplayOptions;
+use embedded_graphics_framebuf::FrameBuf;
 
-use core::f32::consts::PI;
-use libm::{sin, cos};
+use mipidsi::{ Orientation, ColorOrder };
 
 #[allow(unused_imports)]
 use esp_backtrace as _;
@@ -76,23 +78,19 @@ fn main() -> ! {
 
     let di = SPIInterfaceNoCS::new(spi, io.pins.gpio4.into_push_pull_output());
 
-    let display_options = DisplayOptions {
-        orientation: mipidsi::Orientation::PortraitInverted(false),
-        ..Default::default()
-    };
+    let mut display = mipidsi::Builder::ili9342c_rgb565(di)
+        .with_orientation(Orientation::PortraitInverted(false))
+        .with_color_order(ColorOrder::Rgb)
+        .init(&mut delay, core::prelude::v1::Some(reset))
+    .unwrap();
 
-    let mut display = mipidsi::Display::ili9342c_rgb565(di, core::prelude::v1::Some(reset), display_options);
-    display.init(&mut delay).unwrap();
-
-    let default_style = MonoTextStyleBuilder::new()
-        .font(&FONT_10X20)
-        .text_color(RgbColor::BLACK)
-        .build();
+    let mut data = [Rgb565::WHITE; 320 * 240];
+    let mut fbuf = FrameBuf::new(&mut data, 320, 240);
 
     let mut vt;
     let mut x;
     let mut y;
-    for i in 0..13200 {
+    for i in 0..12400 {
         vt = i as f64 / (20.0 * PI as f64);
         if i < 8000 {
             x = (vt - 50.0) * sin(vt);
@@ -101,14 +99,18 @@ fn main() -> ! {
         }
         y = (vt - 50.0) * cos(vt);
         if i < 8000 {
-            Text::with_alignment("'", Point::new((x + 160.0) as i32, (y + 125.0) as i32), default_style,  Alignment::Center)
+            Text::with_alignment("'", Point::new((x + 160.0) as i32, (y + 125.0) as i32), 
+            MonoTextStyleBuilder::new().font(&FONT_10X20).text_color(RgbColor::BLACK).build(),  Alignment::Center)
                 .draw(&mut display)
                 .unwrap();
+            
         } else {
-            Text::with_alignment("|", Point::new((x + 160.0) as i32, (y + 125.0) as i32), default_style,  Alignment::Center)
+            Text::with_alignment("|", Point::new((x + 160.0) as i32, (y + 125.0) as i32), 
+            MonoTextStyleBuilder::new().font(&FONT_10X20).text_color(RgbColor::BLACK).build(),  Alignment::Center)
                 .draw(&mut display)
                 .unwrap();
         }
     }
+
     loop {}
 }
