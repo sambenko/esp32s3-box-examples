79511c95591bc151e6c7bc4a68102a9e50bd6181
sambenko <sam.benko@gmail.com>
2023-11-06 14:57:29 +0100

    main updated for newest esp-hal v0.13.0

--- src/main.rs
+++ src/main.rs
@@ -14,69 +14,60 @@ use embedded_graphics::{
     Drawable,
 };
 
-use esp32s3_hal::{
+use hal::{
     clock::{ClockControl, CpuClock},
     peripherals::Peripherals,
     prelude::*,
-    spi,
-    timer::TimerGroup,
-    Rtc,
+    spi::{master::Spi, SpiMode},
     IO,
     Delay,
 };
 
 use esp_println::println;
 
-use mipidsi::{ Orientation, ColorOrder };
-
-#[allow(unused_imports)]
 use esp_backtrace as _;
-
-use xtensa_lx_rt::entry;
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    let mut system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock240MHz).freeze();
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-    let mut wdt1 = timer_group1.wdt;
 
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::configure(system.clock_control, CpuClock::Clock240MHz).freeze();
+    let mut delay = Delay::new(&clocks);
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
     let sclk = io.pins.gpio7;
     let mosi = io.pins.gpio6;
-    let mut backlight = io.pins.gpio45.into_push_pull_output();
-    backlight.set_high().unwrap();
 
-    let spi = spi::Spi::new_no_cs_no_miso(
+    let dc = io.pins.gpio4.into_push_pull_output();
+    let mut backlight = io.pins.gpio45.into_push_pull_output();
+    let reset = io.pins.gpio48.into_push_pull_output();
+
+    let spi = Spi::new_no_cs_no_miso(
         peripherals.SPI2,
         sclk,
         mosi,
         60u32.MHz(),
-        spi::SpiMode::Mode0,
-        &mut system.peripheral_clock_control,
+        SpiMode::Mode0,
         &clocks,
     );
 
-    let di = SPIInterfaceNoCS::new(spi, io.pins.gpio4.into_push_pull_output());
-    let reset = io.pins.gpio48.into_push_pull_output();
-
-    let mut delay = Delay::new(&clocks);
-
-    let mut display = mipidsi::Builder::ili9342c_rgb565(di)
+    let di = SPIInterfaceNoCS::new(spi, dc);
+    delay.delay_ms(500u32);
+    
+    let mut display = match mipidsi::Builder::ili9342c_rgb565(di)
         .with_display_size(320, 240)
-        .with_orientation(Orientation::PortraitInverted(false))
-        .with_color_order(ColorOrder::Bgr)
-        .init(&mut delay, Some(reset))
-        .unwrap();
+        .with_orientation(mipidsi::Orientation::PortraitInverted(false))
+        .with_color_order(mipidsi::ColorOrder::Bgr)
+        .init(&mut delay, Some(reset)) {
+        Ok(display) => display,
+        Err(_) => {
+            panic!("Display initialization failed");
+        }
+    };
+
+    backlight.set_high().unwrap();
 
     display.clear(Rgb565::WHITE).unwrap();
 
